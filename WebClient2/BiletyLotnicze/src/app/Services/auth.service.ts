946fdc25f489b8842907fdbfb668c0145060b101
lukpro681 <lukapro681@student.polsl.pl>
2024-06-26 01:13:42 +0200

    fixed a little chochlik, now authorization works

--- WebClient2/BiletyLotnicze/src/app/Services/auth.service.ts
+++ WebClient2/BiletyLotnicze/src/app/Services/auth.service.ts
@@ -7,12 +7,12 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:7009/api/Auth'; // Upewnij się, że URL jest poprawny
+  private baseUrl = 'https://localhost:7009/api/Auth'; // Upewnij się, że URL jest poprawny
 
   constructor(private http: HttpClient) { }
 
   register(user: any): Observable<any> {
-    return this.http.post(`http://localhost:7009/api/Auth/register`, user);
+    return this.http.post(`https://localhost:7009/api/Auth/register`, user);
   }
 
   login(credentials: any): Observable<any> {
@@ -20,7 +20,7 @@ export class AuthService {
       'Content-Type': 'application/json'
     });
 
-    return this.http.post(`http://localhost:7009/api/Auth/login`, credentials, { headers, observe: "response" }).pipe(
+    return this.http.post(`https://localhost:7009/api/Auth/login`, credentials, { headers, observe: "response" }).pipe(
       map((response: HttpResponse<any>) => {
         const token = response.headers.get('Authorization');
         return { token };
